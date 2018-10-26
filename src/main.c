1316fc81a7e752fdda9a31c996152b9ed18ef9b3
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-10-26 13:28:04 +0200

    updated description text

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
  * Programmer: Henrik A. Christensen     Date Completed: in progress
  * Instructor: Kurt Nørmark              Class:          Imperative Programming
  *
- * Coming soon...
+ * Write a function that will merge the contents of two sorted arrays (ascending order)
+ * The arrays should be of type double
+ *
+ * The funtion should not assume that both arrays are the same length
+ * The function can assume that one array does not contain two copies of the same value
+ *
+ * The result array should also contain no dublicate values.
 */
 
 #include <stdio.h>
